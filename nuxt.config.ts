af4543b3881ffd8d89f099a571208eb150283395
Valentin Chmara <valentinchmara@gmail.com>
2025-04-24 13:02:18 +0200

    fix: remove nuxt hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
 
   // https://nuxt.com/modules
   modules: [
-    '@nuxthub/core',
     '@nuxt/eslint',
     '@nuxt/ui',
     '@vueuse/nuxt',
@@ -15,11 +14,6 @@ export default defineNuxtConfig({
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2024-07-30',
-
-  // https://hub.nuxt.com/docs/getting-started/installation#options
-  hub: {
-    cache: true,
-  },
 
   // https://eslint.nuxt.com
   eslint: {
